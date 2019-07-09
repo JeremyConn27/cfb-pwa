93211533ae3257118d69959bd6634d34d537062d
JeremyConn27 <jconn27@gmail.com>
2019-07-09 18:21:03 -0400

    added routing

--- cfb-app/src/app/app-routing.module.ts
+++ cfb-app/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { DocsComponent } from './docs/docs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'schedule', component: ScheduleComponent},
+  { path: 'docs', component: DocsComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- cfb-app/src/app/app.module.ts
+++ cfb-app/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { HeaderComponent } from './header/header.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { DocsComponent } from './docs/docs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    HomeComponent,
+    ScheduleComponent,
+    DocsComponent
   ],
   imports: [
     BrowserModule,
