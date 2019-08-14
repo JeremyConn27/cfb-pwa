898d6e80eac7503502b2a4d4a9a670f9a2a7b4fb
JeremyConn27 <jconn27@gmail.com>
2019-08-14 17:40:08 -0400

    updated nav

--- cfb-app/src/app/navigation/navigation.component.ts
+++ cfb-app/src/app/navigation/navigation.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-
   constructor() { }
 
   ngOnInit() {
+  }
+
+  lazyLoadThis(){
+  const targets = document.querySelectorAll('nav');
+
+  const lazyLoad = target => {
+    const io = new IntersectionObserver((entries, observer) => {
+      console.log(entries)
+      entries.forEach(entry => {
+        console.log('😍');
+  
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute('data-lazy');
+  
+          img.setAttribute('src', src);
+          img.classList.add('fade');
+  
+          observer.disconnect();
+        }
+      });
+    });
+
+    io.observe(target)
+  };
+  targets.forEach(lazyLoad);
   }
 
 
