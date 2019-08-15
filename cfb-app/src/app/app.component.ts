5cec574aa0b8ce8b9437f22fc18d8a0253e36b94
JeremyConn27 <jconn27@gmail.com>
2019-08-15 19:44:23 -0400

    fixed header scroll animation

--- cfb-app/src/app/app.component.ts
+++ cfb-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChildren, ElementRef, Renderer2, QueryList, AfterViewInit} from '@angular/core';
 import { fader } from './route-animations';
 import { RouterOutlet, ActivatedRoute} from '@angular/router';
 
@@ -11,8 +11,46 @@ import { RouterOutlet, ActivatedRoute} from '@angular/router';
   ]
 })
 
-export class AppComponent {
+
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'cfb-app';
+  // @ViewChild('header') headerElement: HeaderComponent;
+  // @ViewChildren('header') header: QueryList<any>;
+  el: any;
+  elTwo: any;
+  observer = new IntersectionObserver(entries => {
+  console.log(entries);
+  if (entries[0].boundingClientRect.top < 0) {
+    console.log('Past 20px!');
+    //x.classList.add('stuck')
+      // Array.of(this.el).every(x => console.log(x));
+      for (let x = 0; x < this.el.length; x++) {
+        this.el[x].classList.add('stuck');
+      }
+
+    // this.elTwo.classList.add('stuck');
+  } else {
+    console.log('Not past 20px');
+    for(let x = 0; x < this.el.length; x++){
+      this.el[x].classList.remove('stuck');
+    }
+   // this.elTwo.classList.remove('stuck');
+  }
+  });
+
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.el = document.getElementsByClassName('animated');
+    // this.elTwo = document.querySelector('body > app-root > main > app-schedule > h1');
+    console.log(this.el);
+  }
+
+  ngOnInit() {
+    this.observer.observe(document.querySelector('#pixel-to-watch'));
+  }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
--- cfb-app/src/app/header/header.component.ts
+++ cfb-app/src/app/header/header.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
+})
+@Directive({
+  selector: '[header]'
 })
 export class HeaderComponent implements OnInit {
 
