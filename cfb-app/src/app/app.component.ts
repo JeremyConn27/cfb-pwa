268ab120c372be9f44a4141ec54a16447212ccbc
JeremyConn27 <jconn27@gmail.com>
2019-10-08 23:37:41 -0400

    latest colors

--- cfb-app/src/app/app.component.ts
+++ cfb-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChildren, ElementRef, Renderer2, QueryList, AfterViewInit} from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit} from '@angular/core';
 import { fader } from './route-animations';
 import { RouterOutlet, ActivatedRoute} from '@angular/router';
 
@@ -14,27 +14,20 @@ import { RouterOutlet, ActivatedRoute} from '@angular/router';
 
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'cfb-app';
-  // @ViewChild('header') headerElement: HeaderComponent;
-  // @ViewChildren('header') header: QueryList<any>;
   el: any;
   elTwo: any;
   observer = new IntersectionObserver(entries => {
   console.log(entries);
   if (entries[0].boundingClientRect.top < 0) {
     console.log('Past 20px!');
-    //x.classList.add('stuck')
-      // Array.of(this.el).every(x => console.log(x));
       for (let x = 0; x < this.el.length; x++) {
         this.el[x].classList.add('stuck');
       }
-
-    // this.elTwo.classList.add('stuck');
   } else {
     console.log('Not past 20px');
     for(let x = 0; x < this.el.length; x++){
       this.el[x].classList.remove('stuck');
     }
-   // this.elTwo.classList.remove('stuck');
   }
   });
 
@@ -43,7 +36,6 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
     this.el = document.getElementsByClassName('animated');
-    // this.elTwo = document.querySelector('body > app-root > main > app-schedule > h1');
     console.log(this.el);
   }
 
